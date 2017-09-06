6ef92ff216f2c932faf0e9ee6e5e959ed827add8
Harshal <harshallpandit@gmail.com>
2017-09-06 13:33:28 -0400

    Pause functionality

--- src/Breakout.java
+++ src/Breakout.java
@@ -1,16 +1,21 @@
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Stack;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class Breakout extends JPanel {
@@ -24,6 +29,12 @@ public class Breakout extends JPanel {
 	Paddle paddle;
 	Brick brick;
 	Clock clock;
+	JButton replay, undo, pause, resume;
+	static Stack<Ball> ballObjects;
+	static Stack<Paddle> paddleObjects;
+	static Stack<Clock> clockObjects;
+	static Stack<Brick> brickObjects;
+	int breakLoop = 0;
 	
 	
 	Breakout()
@@ -38,6 +49,26 @@ public class Breakout extends JPanel {
 		this.clock = clock;
 		this.addKeyListener(paddle);
 		setFocusable(true);
+		replay = new JButton("Replay");
+		replay.setFocusable(false);
+		undo = new JButton("undo");
+		undo.setFocusable(false);
+		pause = new JButton("Pause");
+		pause.setFocusable(false);
+		resume = new JButton("Resume");
+		resume.setFocusable(false);
+		this.add(replay);
+		this.add(undo);
+		this.add(pause);
+		this.add(resume);
+		ballObjects = new Stack<Ball>();
+		paddleObjects = new Stack<Paddle>();
+		clockObjects = new Stack<Clock>();
+		brickObjects = new Stack<Brick>();
+		
+		this.ball.registerBall();
+		this.clock.registerClock();
+		
 	}
 	
 	public void paint(Graphics g)
@@ -70,13 +101,30 @@ public class Breakout extends JPanel {
 	
 	public void startGame()
 	{		
-		ball.registerBall();
-		clock.registerClock();
+		pause.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				breakLoop = 1;
+			}
+		});
+		
+		resume.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+			//	startGame() with current Instances;
+			}
+		});
+		
 		
 		while(gameIsOn)
 		{
 			ball.moveBall();
 			brick.brickCollide(ball);
+			storeInstance(ball, paddle, clock, brick);
 			BreakoutObservable observable = new BreakoutObservable(paddle);
 			observable.notifyObservers();
 			
@@ -92,8 +140,15 @@ public class Breakout extends JPanel {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
+			if(breakLoop == 1)
+			{
+				break;
+			}
+			
 			repaint();
 		}
+		
 		ball.unregisterBall();
 		clock.unregisterClock();
 	}
@@ -111,6 +166,15 @@ public class Breakout extends JPanel {
 			}
 		}
 		return true;
+	}
+	
+	public void storeInstance(Ball ball, Paddle paddle, Clock clock, Brick brick)
+	{
+
+		ballObjects.push(ball);
+		paddleObjects.push(paddle);
+		clockObjects.push(clock);
+		brickObjects.push(brick);
 	}
 	
 	/*
