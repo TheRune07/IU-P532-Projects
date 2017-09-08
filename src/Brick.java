ef38566f5fcf54a36f2f7c05852a7e7062cea6b1
anchal <ankhande@iu.edu>
2017-09-08 15:15:37 -0400

    All constants moved to constants file

--- src/Brick.java
+++ src/Brick.java
@@ -3,10 +3,10 @@ import java.awt.Graphics2D;
 
 public class Brick implements Sprite, Cloneable {
 	
-	int[][] bricksX = new int[6][19];
-	int[][] bricksY = new int[6][19];
-	static final int BRICK_WIDTH = 75;
-	static final int BRICK_HEIGHT = 10;
+	int[][] bricksX = new int[Constants.BRICK_ROW][Constants.BRICK_COLUMN];
+	int[][] bricksY = new int[Constants.BRICK_ROW][Constants.BRICK_COLUMN];
+	int BRICK_WIDTH = Constants.BRICK_WIDTH;
+	int BRICK_HEIGHT = Constants.BRICK_HEIGHT;
 	Brick cloneBrick;
 	
 	Brick(Brick brick)
@@ -18,9 +18,9 @@ public class Brick implements Sprite, Cloneable {
 	{
 		int brx = 0;
 		int bry = 0;
-		for(int i = 0; i < 6; i++) // put in initializeBricks
+		for(int i = 0; i < Constants.BRICK_ROW; i++) // put in initializeBricks
 		{
-			for(int j = 0; j < 19; j++)
+			for(int j = 0; j < Constants.BRICK_COLUMN; j++)
 			{
 				bricksX[i][j] = brx + 20;
 				bricksY[i][j] = bry + 40;
@@ -76,9 +76,9 @@ public class Brick implements Sprite, Cloneable {
 	
 	public void brickCollide(Ball ball) 
 	{
-		for(int i = 0; i < 6; i++)
+		for(int i = 0; i < Constants.BRICK_ROW; i++)
 		{
-			for(int j = 0; j < 19; j++)
+			for(int j = 0; j < Constants.BRICK_COLUMN; j++)
 			{
 				if(bottomCollision(ball.getBx(), ball.getBy(), bricksX[i][j], bricksY[i][j])) {
 					ball.setMoveY(1);
@@ -111,13 +111,13 @@ public class Brick implements Sprite, Cloneable {
 	@Override
 	public void draw(Graphics2D g2d) {
 		// TODO Auto-generated method stub
-		for(int i = 0; i < 6; i++)
+		for(int i = 0; i < Constants.BRICK_ROW; i++)
 		{
-			for(int j = 0; j < 19; j++)
+			for(int j = 0; j < Constants.BRICK_COLUMN; j++)
 			{
 				if(bricksX[i][j] != -1 && bricksX[i][j] != -1)
 				{
-					g2d.setColor(Color.ORANGE);
+					g2d.setColor(Constants.BRICK_COLOR);
 					g2d.fillRect(bricksX[i][j], bricksY[i][j], BRICK_WIDTH, BRICK_HEIGHT);
 				}
 			}
