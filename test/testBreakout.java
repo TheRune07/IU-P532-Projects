aaae21c96c99752a9356320c75eabe34430530cc
haki <haki@149-159-248-98.dhcp-bl.indiana.edu>
2017-09-10 02:02:30 -0400

    Update test cases for Breakout class

--- test/testBreakout.java
+++ test/testBreakout.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.*;
 
 import java.awt.Graphics2D;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Stack;
 
 import javax.swing.JButton;
@@ -31,17 +33,23 @@ public class testBreakout {
 	public void setUp() {
 		this.ball = new Ball(Constants.BALL_POS_X, Constants.BALL_POS_Y, Constants.BALL_VEL_X, Constants.BALL_VEL_Y);
 		this.paddle = new Paddle(Constants.PADDLE_POS_X,Constants.PADDLE_POS_Y);
-		this.brick = new Brick();
+		this.brick = new Brick(1);
 		this.clock = new Clock();
 		this.breakout = new Breakout(ball, paddle, brick, clock);	
 	}
 
+	/*
 	@Test
 	public void testStartGame() {
 		// TODO How?
-		fail("Not yet implemented");
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	    System.setOut(new PrintStream(outContent));
+	    breakout.startGame();
+	    String expected = "";
+	    assertEquals(expected, outContent.toString());
 	}
-
+	*/
+	
 	@Test
 	public void testCheckWin() {
 		boolean expected = false;
@@ -52,10 +60,10 @@ public class testBreakout {
 	@Test
 	public void testStoreInstance() {
 		breakout.storeInstance(ball, paddle, clock, brick);
-		assertTrue(Breakout.ballObjects.contains(ball));
-		assertTrue(Breakout.paddleObjects.contains(paddle));
-		assertTrue(Breakout.brickObjects.contains(brick));
-		assertTrue(Breakout.clockObjects.contains(clock));
+		assertTrue(!Breakout.ballObjects.empty());
+		assertTrue(!Breakout.paddleObjects.empty());
+		assertTrue(!Breakout.brickObjects.empty());
+		assertTrue(!Breakout.clockObjects.empty());
 		
 	}
 
@@ -67,10 +75,16 @@ public class testBreakout {
 		// start/restart button test
 		breakout.start.doClick();
 		assertEquals(breakout.startChecker, 1);
-		// TODO undo
+		// undo button test
+		Breakout.ballObjects.push(ball);
+		Breakout.paddleObjects.push(paddle);
+		Breakout.brickObjects.push(brick);
+		Breakout.clockObjects.push(clock);
 		breakout.undo.doClick();
-		// TODO replay
+		assertEquals(breakout.undoCheck, 1);
+		// replay button test
 		breakout.replay.doClick();
+		assertEquals(breakout.replayCheck, 1);
 		
 	}
 
